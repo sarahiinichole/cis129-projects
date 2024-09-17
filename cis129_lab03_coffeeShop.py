1d85933dd602ce0f032b78aec2c02bb8f234929a
sarahiinichole <herediasarah091@gmail.com>
2024-09-16 20:49:56 -0700

    Update cis129_lab03_coffeeShop.py

--- cis129_lab03_coffeeShop.py
+++ cis129_lab03_coffeeShop.py
@@ -1,55 +1,88 @@
-Main   
-     
-        Declare String tax
-        Declare Integer 6%
-        Declare Real .78        
-        
-        //Get input from the customer 
-        Display "What are you purchasing?"
-        Input Coffee
-        Display "Please enter the number of items bought"
-        Input 1
-        Display "Please enter the price of the item"
-        Input 5.00
-        
-        //Calculate the total cost
-        Declare Real totalCost
-        5.00 = 1 * 5.00
-       
-        //Get input from the customer 
-        Display "What are you purchasing?"
-        Input Muffin
-        Display "Please enter the number of items bought"
-        Input 2
-        Display "Please enter the price of the item"
-        Input 4.00
-        
-        //Calculate the total cost
-        Declare Real totalCost
-        8.00 = 2 * 4.00
-        
-        //Display the receipt to the customer
-        Display "------ Receipt ------"
-        Display "Item Purchased: ", nameOfItem
-        Display "Number purchased: ", numberOfItemsPurchased
-        Display "Price of Each Item: ", priceOfItem
-        Display "Total Cost: 13.78 ", totalCost
 
-        Receipt: 
-        ***************************************
-        My Coffee and Muffin Shop
-        Number of coffees bought?
-        1
-        Number of muffins bought?
-        2
-        ***************************************
 
-        ***************************************
-        My Coffee and Muffin Shop Receipt
-        1 Coffee at $5 each: $ 5.00
-        2 Muffins at $4 each: $ 8.00
-        6% tax: $ .78
-        ---------
-        Total: $ 13.78
-        ***************************************
-End Main
+
+# 1) Black Coffee 
+# 2) Expresso 
+# 3) Latte 
+# 4) Cappicino 
+
+
+# ask name 
+# ask coffee type 
+# ask quantity 
+# display total 
+
+prices = {
+    1: 2.00, 
+    2: 3.15, 
+    3: 4.00,
+    4: 4.99
+}
+
+class CoffeeShop: 
+    global data 
+    data = {}
+
+    def promptUser(self):
+        print("\nHello, welcome to our coffee shop, what is your name?")
+        name =- input("")
+
+        print("\n\tOur menu\n\n\t1) Black coffee\n\t2) Expresso\n\t3) Latte\n\t4) Cappicino\n\t5)")
+        order = int(input("\t"))
+
+        print("\Quanitity: ")
+        quanitity = int(input())
+
+        if order == 1: # black coffee 
+            total = prices[1] * quanitity
+            data['name'] = name 
+            data['type'] = 'Black Coffee'
+            data['total'] = str(math.floor(total))
+
+
+        elif  order == 2:
+            total = prices[2] * quanitity
+            data['name'] = name 
+            data['type'] = 'Expresso' 
+            data['total'] = str(math.floor(total))
+
+        elif  order == 3:
+            total = prices[3] * quanitity
+            data['name'] = name 
+            data['type'] = 'Latte' 
+            data['total'] = str(math.floor(total))
+
+        elif  order == 4:
+            total = prices[4] * quanitity
+            data['name'] = name 
+            data['type'] = 'Cappicino' 
+            data['total'] = str(math.floor(total))
+
+        return data 
+    
+    def recipet(self, promptUser):
+
+        print("Recipet for customer")
+
+        name = promtUser['name']
+        type = promptUser['type']
+        total = promptUser['total']
+
+        print("Name: " + name 
+              +"\nCoffee type: " + type
+              +"\nTotal: $" + total + "\n")
+        
+
+
+coffee = CoffeeShop()
+coffee.recipet(coffee.promptUser())
+
+print("\nDo you want to calculate for another customer?")
+choice = input("")
+
+if choice != "No":
+    coffee.recipet(coffee.promptUser())
+else: 
+    exit(0)
+
+
